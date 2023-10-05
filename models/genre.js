99e325e07c0b8779c592e6a569e864a032b2637c
Misael Guerrero <mguerrerowest@gmail.com>
2023-10-05 11:13:01 -0400

    Fix typos and rename genere.js to genre.js

--- models/genre.js
+++ models/genre.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const genereSchema = new Schema({
+const genreSchema = new Schema({
     name: { type: String, required: true, minLength: 3, maxLength: 100 }
 });
 
-// Virtual for genere's URL
-genereSchema.virtual("url").get(function () {
+// Virtual for genre's URL
+genreSchema.virtual("url").get(function () {
     // We don't use an arrow function as we'll need the this object
-    return `/catalog/genere/${this._id}`;
+    return `/catalog/genre/${this._id}`;
   });
 
 // Export model
-module.exports = mongoose.model("Genere", genereSchema);
+module.exports = mongoose.model("Genre", genreSchema);
