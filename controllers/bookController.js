4587815574c8f206b9b71d86f7b3ef002884297b
Misael Guerrero <mguerrerowest@gmail.com>
2023-10-10 13:21:40 -0400

    Import all the models

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,8 @@
 const Book = require("../models/book");
+const Author = require("../models/author");
+const Genre = require("../models/genre");
+const BookInstance = require("../models/bookinstance");
+
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
