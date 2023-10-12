5c5ae134111dad79ca611fe025c606b2738f33b6
Misael Guerrero <mguerrerowest@gmail.com>
2023-10-12 10:44:26 -0400

    Create function that renders the genre from view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,8 @@
 const Genre = require("../models/genre");
 const Book = require("../models/book");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
 
 // Display list of all Genre.
 exports.genre_list = asyncHandler(async (req, res, next) => {
@@ -32,9 +34,9 @@ exports.genre_detail = asyncHandler(async (req, res, next) => {
 
 
 // Display Genre create form on GET.
-exports.genre_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre create GET");
-});
+exports.genre_create_get = (req, res, next) => {
+  res.render("genre_form", { title: "Create Genre" });
+};
 
 // Handle Genre create on POST.
 exports.genre_create_post = asyncHandler(async (req, res, next) => {
